3e9495796d405bdc703731927e112eb566dfc3c6
Jain Waldrip <jkwaldri@iu.edu>
2013-12-03 11:14:56 -0500

    Create Marc Editor test - New Item

--- spec/describe/marc_editor_spec.rb
+++ spec/describe/marc_editor_spec.rb
@@ -40,4 +40,19 @@ describe 'The Marc Editor' do
       results[:pass?].should be_true
     end
   end
+
+  context 'with a new item record' do
+    let(:item_editor)           { OLE_QA::Framework::OLELS::Item_Editor.new(@ole) }
+    let(:item_record)           { OpenStruct.new(:item_hsh => {
+                                    :item_type        => 'Book',
+                                    :item_status      => 'Available',
+                                    :barcode          => OLE_QA::Framework::Bib_Factory.barcode })
+                                }
+
+    it 'creates a new item record' do
+      results = create_item(item_editor, item_record.item_hsh)
+      results[:error].should be_nil
+      results[:pass?].should be_true
+    end
+  end
 end
