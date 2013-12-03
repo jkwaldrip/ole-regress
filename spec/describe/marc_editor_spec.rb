f6bf234f0c40aa719bd0f552a394c56c9595d21e
Jain Waldrip <jkwaldri@iu.edu>
2013-12-03 11:09:51 -0500

    Create Marc Editor test - New Holdings

--- spec/describe/marc_editor_spec.rb
+++ spec/describe/marc_editor_spec.rb
@@ -7,10 +7,10 @@ describe 'The Marc Editor' do
 
   context 'with a new bib record' do
     let(:bib_editor)            { OLE_QA::Framework::OLELS::Bib_Editor.new(@ole) }
-    let(:marc_record)           { OpenStruct.new(:bib_ary => 
+    let(:bib_record)           { OpenStruct.new(:bib_ary => 
                                     [{:tag    => '100',
                                       :value  => '|a' + OLE_QA::Framework::String_Factory.alphanumeric},
-                                    {:tag     => '245',
+                                      {:tag     => '245',
                                       :value  => '|a' + OLE_QA::Framework::String_Factory.alphanumeric}])
                                  }
 
@@ -20,9 +20,24 @@ describe 'The Marc Editor' do
     end
     
     it 'enters a title and author' do
-      results = create_bib(bib_editor, marc_record.bib_ary)
-      results[:pass?].should be_true
+      results = create_bib(bib_editor, bib_record.bib_ary)
       results[:error].should be_nil      
+      results[:pass?].should be_true
+    end
+  end
+
+  context 'with a new holdings record' do
+    let(:instance_editor)       { OLE_QA::Framework::OLELS::Instance_Editor.new(@ole) }
+    let(:holdings_record)       { OpenStruct.new(:instance_hsh => {
+                                    :location         => 'B-EDUC/BED-STACKS',
+                                    :call_number      => OLE_QA::Framework::Bib_Factory.call_number,
+                                    :call_number_type => 'LCC' })
+                                }
+
+    it 'creates a new instance record' do
+      results = create_instance(instance_editor, holdings_record.instance_hsh)
+      results[:error].should be_nil
+      results[:pass?].should be_true
     end
   end
 end
