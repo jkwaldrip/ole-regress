339313c6da4f2fe0b594f2eb9ffc36765fff6406
jkwaldri <jkwaldri@indiana.edu>
2014-01-02 18:33:19 -0500

    Create cuke-ified instance editor test

--- features/step_definitions/marc_editor_steps.rb
+++ features/step_definitions/marc_editor_steps.rb
@@ -17,27 +17,54 @@ Given /^I am using the Marc Editor$/ do
   @bib_editor.open
 end
 
-When /^I add a random title$/ do
-  random_title = '|a' + OLE_QA::Framework::String_Factory.alpha(8)
+When /^I enter a title$/ do
+  title = '|a' + OLE_QA::Framework::String_Factory.alpha(8).capitalize
   @bib_editor.data_line.tag_field.when_present.set('245')
-  @bib_editor.data_line.data_field.when_present.set(random_title)
+  @bib_editor.data_line.data_field.when_present.set(title)
 end
 
-And /^I add a random author$/ do
-  # Make a string of random characters vaguely resembling a name.
-  first  = OLE_QA::Framework::String_Factory.alpha(4).capitalize
-  last   = OLE_QA::Framework::String_Factory.alpha(8).capitalize
-  author = '|a' + first + ' ' + last
-  # Add another line to the editor.
-  @bib_editor.data_line.add_button.click
+When /^I enter an author$/ do
+  author = '|a' + OLE_QA::Framework::String_Factory.alpha(4).capitalize
+  author += ' ' + OLE_QA::Framework::String_Factory.alpha(8).capitalize
+  @bib_editor.data_line.add_button.when_present.click
   @bib_editor.data_line.line_number += 1
-  @bib_editor.data_line.tag_field.wait_until_present
-  # Now add the author.
-  @bib_editor.data_line.tag_field.set('100')
+  @bib_editor.data_line.tag_field.when_present.set('100')
   @bib_editor.data_line.data_field.when_present.set(author)
 end
 
-Then /^I can save the record$/ do
+Then /^I can save the bib record$/ do
   save_msg = @bib_editor.save_record
+  save_msg.should =~ /success/
+end
+
+When /^I create a bib record$/ do
+  steps %{
+    When I enter a title
+    And I enter an author
+    Then I can save the bib record
+  }
+end
+
+When /^I add an instance record$/ do
+  @instance_editor = OLE_QA::Framework::OLELS::Instance_Editor.new(@ole)
+  @bib_editor.holdings_link(1).when_present.click
+  @instance_editor.wait_for_page_to_load
+end
+
+When /^I enter a location (.*)$/ do |location|
+  @instance_editor.location_field.when_present.set(location)
+end
+
+When /^I enter a call number$/ do
+  call_number = OLE_QA::Framework::Bib_Factory.call_number
+  @instance_editor.call_number_field.when_present.set(call_number)
+end
+
+When /^I select a call number type$/ do
+  @instance_editor.call_number_type_selector.when_present.select_value('LCC')
+end
+
+Then /^I can save the instance record$/ do
+  save_msg = @instance_editor.save_record
   save_msg.should =~ /success/
 end
