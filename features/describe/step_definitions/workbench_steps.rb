e7d242b3e71eb7d40089e50ef4a53c3061a9ae7a
Jain Waldrip <jkwaldri@iu.edu>
2014-01-10 17:11:51 -0500

    Add describe workbench test feature & steps
    Cannot verify barcode search is functional due to current
    problems with the Describe Workbench itself.  All other
    tests were successful on preliminary runs.

---
+++ features/describe/step_definitions/workbench_steps.rb
@@ -0,0 +1,59 @@
+#  Copyright 2005-2013 The Kuali Foundation
+#
+#  Licensed under the Educational Community License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#    http://www.opensource.org/licenses/ecl2.php
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License
+
+Given /^I (?:am using|use) the (?:Describe )?Workbench$/ do
+  @workbench = OLE_QA::Framework::OLELS::Describe_Workbench.new(@ole)
+  @workbench.open
+end
+
+When /^I search for an? (bib|instance|holdings|item) record$/ do |type|
+  @workbench.wait_for_page_to_load
+  type.strip!
+  case type
+    when 'bib'
+      @workbench.doc_type_bib.when_present.set
+    when 'instance','holdings'
+      @workbench.doc_type_holdings.when_present.set
+    when 'item'
+      @workbench.doc_type_item.when_present.set
+  end
+  @workbench.wait_for_page_to_load
+end
+
+When /^I enter the ([A-Za-z\s]+) in the (first|second) search field$/ do |term,line|
+  search_field = @workbench.send("search_field_#{numerize(line)}".to_sym)
+  search_field.when_present.set(@resource[keyify(term)])
+end
+
+When /^I set the (first|second) search selector to (.*)$/ do |line,value|
+  case line
+    when 'first'
+      selector = @workbench.search_field_selector_1
+    when 'second'
+      selector = @workbench.search_field_selector_2
+  end
+  value.strip!
+  Watir::Wait.until {selector.include?(value)}
+  selector.select(value)
+end
+
+When /^I click (?:the )?(search|clear)(?: button)?$/ do |which|
+  button = @workbench.send(keyify("#{which}_button"))
+  button.click
+  @workbench.wait_for_page_to_load
+end
+
+Then /^I (?:should )?see the ([A-Za-z\s]+) in the search results$/ do |term|
+  @workbench.result_present?(@resource[keyify(term)]).should be_true
+end
---
+++ features/describe/workbench.feature
@@ -0,0 +1,37 @@
+Feature:  Describe Workbench
+
+  Background:
+    Given I have a resource
+    Given I use the Marc Editor
+    Given I create a bib record
+    Given I create an instance record
+    Given I create an item record
+    Given I am using the Describe Workbench
+
+  Scenario:  Find a Bib by Title
+    When I search for a bib record
+    And I enter the title in the first search field
+    And I set the first search selector to Title
+    And I click search
+    Then I see the title in the search results
+
+  Scenario:  Find a Bib by Author
+    When I search for a bib record
+    And I enter the author in the first search field
+    And I set the first search selector to Author
+    And I click search
+    Then I see the author in the search results
+
+  Scenario:  Find a Holdings by Call Number
+    When I search for a holdings record
+    And I enter the call number in the first search field
+    And I set the first search selector to Call Number
+    And I click search
+    Then I see the call number in the search results
+
+  Scenario:  Find an Item by Barcode
+    When I search for an item record
+    And I enter the barcode in the first search field
+    And I set the first search selector to Item Barcode
+    And I click search
+    Then I see the barcode in the search results
