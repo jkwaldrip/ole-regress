c586de475b33e197efb96888164ddc7820ba497e
Jain Waldrip <jkwaldri@iu.edu>
2014-01-23 12:55:26 -0500

    Remove RSpec before/after hooks from spec/spec_helper.rb
    These hooks seem to be triggered only on the first run-through.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,23 +6,3 @@ require 'lib/ole-regress.rb'
 # Require shared contexts.
 require 'spec/shared.rb'
 Dir['spec/*/shared.rb'].sort.each {|file| require file}
-
-RSpec.configure do |config|
-
-  # Initiate a new OLE QA Framework Session before each spec.
-  config.before(:all) do
-    opts = OLE_QA::RegressionTest::Options
-    @ole = OLE_QA::Framework::Session.new(opts)
-  end
-
-  config.after(:all) do
-    @ole.browser.close
-  end
-
-  config.after(:each) do
-    if example.exception != nil && @ole.class == OLE_QA::Framework::Session
-      filename = "#{example.description.to_s.capitalize} - #{Time.now.strftime("%Y\-%m\-%d \- %I-%M-%S %p %Z")}.png"
-      @ole.browser.screenshot.save('screenshots/' + filename)
-    end
-  end
-end
