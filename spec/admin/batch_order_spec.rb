63c359f003656dc5ece44c142db3e13aa9408591
Jain Waldrip <jkwaldri@iu.edu>
2014-01-30 14:53:46 -0500

    Create batch order spec up to load summary search

--- spec/admin/batch_order_spec.rb
+++ spec/admin/batch_order_spec.rb
@@ -12,3 +12,85 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+require 'rspec'
+require 'spec_helper.rb'
+
+describe 'The Order Record Batch Process' do
+
+  include OLE_QA::RegressionTest::Assertions
+
+  let(:batch_process)                     {OLE_QA::Framework::OLELS::Batch_Process.new(@ole)}
+  let(:profile_lookup)                    {OLE_QA::Framework::OLELS::Batch_Profile_Lookup.new(@ole)}
+  let(:load_summary_lookup)               {OLE_QA::Framework::OLEFS::Load_Summary_Lookup.new(@ole)}
+  let(:job_lookup)                        {OLE_QA::Framework::OLELS::Batch_Job_Details.new(@ole)}
+  let(:job_report)                        {OLE_QA::Framework::OLELS::Batch_Job_Report.new(@ole)}
+
+  before :all do
+    eocr       = OLE_QA::RegressionTest::EOCR.new
+    @batch_job = OpenStruct.new(
+        :job_name     => OLE_QA::Framework::String_Factory.alphanumeric(12),
+        :mrc_file     => eocr.mrc_file,
+        :edi_file     => eocr.edi_file,
+        :file_name    => eocr.filename
+    )
+  end
+
+  it 'opens a new batch process' do
+    batch_process.open
+  end
+
+  it 'selects the Order Record Import profile' do
+    batch_process.profile_search_icon.when_present.click
+    profile_lookup.wait_for_page_to_load
+    profile_lookup.profile_type_selector.when_present.select('Order Record Import')
+    profile_lookup.search_button.when_present.click
+    Watir::Wait.until {profile_lookup.text_in_results('Test_Order_Import').present?}.should be_true
+    profile_lookup.return_by_text('Test_Order_Import').click
+    batch_process.wait_for_page_to_load
+  end
+
+  it 'gives the job a name' do
+    batch_process.name_field.when_present.set(@batch_job.job_name)
+  end
+
+  it 'selects the .mrc file' do
+    batch_process.marc_file_field.when_present.set(@batch_job.mrc_file)
+  end
+
+  it 'selects the .edi file' do
+    batch_process.edi_file_field.when_present.set(@batch_job.edi_file)
+  end
+
+  it 'runs the job' do
+    batch_process.run_button.when_present.click
+    batch_process.wait_for_page_to_load
+    batch_process.message.when_present.text.should =~ /successfully saved/
+  end
+
+  # Dismiss job details page
+
+  it 'generates a load summary' do
+    load_summary_lookup.open
+    assert {
+      load_summary_lookup.user_id_field.when_present.clear
+      load_summary_lookup.load_profile_selector.when_present.select('YBP')
+      load_summary_lookup.filename_field.set(@batch_job.file_name)
+      load_summary_lookup.search_button.click
+      load_summary_lookup.wait_for_page_to_load
+      load_summary_lookup.text_in_results?(@batch_job.file_name)
+    }
+    load_summary_lookup.text_in_results?(@batch_job.file_name).should be_true
+  end
+
+  context 'generates a load report' do
+    it 'with a document ID' do
+      @batch_job.load_report_id = load_summary_lookup.doc_link_by_text(@batch_job.file_name).text.strip
+      @batch_job.load_report_id.should =~ /\d+/
+    end
+
+    # verify successes/failures exist
+    # save no. of records, successes, failures to struct
+    # verify links to bibs
+    # verify links to POs
+  end
+end
