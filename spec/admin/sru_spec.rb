e48323386205ba5751fff6d99781846b6b68ea13
jkwaldri <jkwaldri@indiana.edu>
2014-02-25 12:02:24 -0500

    Create SRU spec outline

---
+++ spec/admin/sru_spec.rb
@@ -0,0 +1,45 @@
+require 'rspec'
+require 'spec_helper.rb'
+
+describe 'The OLE SRU function' do
+
+  context 'starts with' do
+    it 'one Marc record' do
+
+    end
+
+    it 'two Marc records' do
+
+    end
+  end
+
+  context 'searches by title' do
+    it 'with a general search' do
+
+    end
+
+    it 'with an exact match' do
+
+    end
+  end
+
+  context 'searches by author' do
+    it 'with a general search' do
+
+    end
+
+    it 'with an exact match' do
+
+    end
+  end
+
+  context 'searches by year' do
+    it '>= target value' do
+
+    end
+
+    it '<= target value' do
+
+    end
+  end
+end
