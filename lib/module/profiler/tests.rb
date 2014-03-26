d88dbc1f9d8dd2e5228f187ceca33bc63da9ef56
Jain Waldrip <jkwaldri@iu.edu>
2014-03-26 09:50:06 -0400

    Create checkout/checkin test

--- lib/module/profiler/tests.rb
+++ lib/module/profiler/tests.rb
@@ -117,6 +117,121 @@ module OLE_QA::Profiler
     #   then return it.
     #
     def checkout_checkin
+      # Create new patron.
+      patron        = OpenStruct.new(OLE_QA::Framework::Patron_Factory.new_patron)
+      patron_lookup = OLE_QA::Framework::OLELS::Patron_Lookup.new(@ole)
+      patron_page   = OLE_QA::Framework::OLELS::Patron.new(@ole)
+      patron_lookup.open
+      patron_lookup.create_new.when_present.click
+      patron_page.wait_for_page_to_load
+      patron_page.wait_for_page_to_load
+      patron_page.barcode_field.when_present.set(patron.barcode)
+      patron_page.borrower_type_selector.select(patron.borrower_type)
+      patron_page.first_name_field.set(patron.first)
+      patron_page.last_name_field.set(patron.last)
+      patron_page.address_line.address_source_selector.when_present.select('Operator')
+      patron_page.address_line.details_link.click
+      patron_page.address_line.line_1_field.when_present.set(patron.address)
+      patron_page.address_line.city_field.set(patron.city)
+      patron_page.address_line.state_selector.select(patron.state)
+      patron_page.address_line.postal_code_field.set(patron.postal_code)
+      patron_page.address_line.country_selector.select('United States')
+      patron_page.address_line.active_checkbox.set(true)
+      patron_page.address_line.add_button.click
+      patron_page.address_line.line_number = 2
+      patron_page.address_line.line_1_field.wait_until_present
+      patron_page.phone_line.phone_number_field.when_present.set(patron.phone)
+      patron_page.phone_line.country_selector.select('United States')
+      patron_page.phone_line.active_checkbox.set(true)
+      patron_page.phone_line.add_button.click
+      patron_page.phone_line.line_number = 2
+      patron_page.phone_line.phone_number_field.wait_until_present
+      patron_page.email_line.email_address_field.when_present.set(patron.email)
+      patron_page.email_line.active_checkbox.set(true)
+      patron_page.email_line.add_button.click
+      patron_page.email_line.line_number = 2
+      patron_page.email_line.email_address_field.wait_until_present
+      patron_page.submit_button.click
+      patron_page.wait_for_page_to_load
+      verify {patron_page.message.when_present.text =~ /success/}
+
+      # Create item record.
+      bib_ary     = [{
+                      :tag      => '245',
+                      :ind_1    => '',
+                      :ind_2    => '',
+                      :value    => '|a' + OLE_QA::Framework::String_Factory.alphanumeric(12)
+                     },
+                    {
+                      :tag      => '100',
+                      :ind_1    => '',
+                      :ind_2    => '',
+                      :value    => '|a' + OLE_QA::Framework::String_Factory.alphanumeric(14)
+                    }]
+      bib_editor = OLE_QA::Framework::OLELS::Bib_Editor.new(@ole)
+      bib_editor.open
+      create_bib(bib_editor,bib_ary)
+      instance  = {   :location         => 'B-EDUC/BED-STACKS',
+                      :call_number      => OLE_QA::Framework::Bib_Factory.call_number,
+                      :call_number_type => 'LCC'
+                    }
+      instance_editor = OLE_QA::Framework::OLELS::Instance_Editor.new(@ole)
+      create_instance(instance_editor,instance)
+      @ole.browser.windows[-1].close
+      @ole.browser.windows[0].use
+      item      = {   :item_type        => 'Book',
+                      :item_status      => 'Available',
+                      :barcode          => OLE_QA::Framework::Bib_Factory.barcode   
+                  }
+      item_editor = OLE_QA::Framework::OLELS::Item_Editor.new(@ole)
+      create_item(item_editor,item)
+      @ole.browser.windows[-1].close
+      @ole.browser.windows[0].use
+
+      # Login as dev2.
+      page = OLE_QA::Framework::Page.new(@ole,@ole.url)
+      page.open
+      page.login('dev2')
+
+      # Loan the item.
+      loan_page = OLE_QA::Framework::OLELS::Loan.new(@ole)
+      loan_page.open
+      loan_page.circulation_desk_selector.when_present.select('BL_EDUC')
+      loan_page.circulation_desk_yes.click if verify(5) {loan_page.circulation_desk_yes.present?}
+      loan_page.loan_popup_box.wait_while_present if verify(5) {loan_page.loan_popup_box.present?}
+      verify {loan_page.circulation_desk_selector.selected?('BL_EDUC')}
+      loan_page.wait_for_page_to_load
+      loan_page.patron_field.set("#{patron.barcode}\n")
+      loan_page.item_field.when_present.set("#{item[:barcode]}\n")
+      loan_page.loan_button.when_present.click if verify(10) {loan_page.loan_popup_box.present?}
+      verify(10) {loan_page.item_barcode_link(1).when_present.text.strip.include?(item[:barcode])}
+
+      # Return the item.
+      return_page = OLE_QA::Framework::OLELS::Return.new(@ole)
+      loan_page.return_button.when_present.click
+      return_page.item_field.wait_until_present
+      date            = Time.now.strftime("%m/%d/%Y")
+      time            = Time.now.strftime("%k:%M")
+      expected_str    = Time.now.strftime("%m/%d/%Y %I:%M %p")
+      checkin        = OpenStruct.new(:date => date, :time => time, :expected_str => expected_str)
+      return_page.checkin_date_field.set(checkin.date)
+      return_page.checkin_time_field.set(checkin.time)
+      return_page.item_field.set("#{item[:barcode]}\n")
+      if verify(5) {return_page.checkin_message_box.present?}
+        return_page.return_button.click
+        return_page.checkin_message_box.wait_while_present
+      end
+      Watir::Wait.until {@ole.browser.windows.count > 1}
+      @ole.browser.windows[-1].use
+      @ole.browser.windows[0].close
+      return_page.items_returned_toggle.wait_until_present
+      verify(10) {return_page.item_barcode_link(1).text.include?(item[:barcode])}
+      verify(10) {return_page.item_checkin_date.text.include?(checkin.expected_str)}
+      return_page.end_session_button.when_present.click
+
+      #Logout
+      page.open
+      page.logout
     end
 
     # Create a bib record and retrieve it through the 
@@ -124,6 +239,23 @@ module OLE_QA::Profiler
     #   search methods.
     #
     def sru_response
+      # Create bib record.
+      bib_ary     = [{
+                         :tag      => '245',
+                         :ind_1    => '',
+                         :ind_2    => '',
+                         :value    => '|a' + OLE_QA::Framework::String_Factory.alphanumeric(12)
+                     },
+                     {
+                         :tag      => '100',
+                         :ind_1    => '',
+                         :ind_2    => '',
+                         :value    => '|a' + OLE_QA::Framework::String_Factory.alphanumeric(14)
+                     }]
+      bib_editor = OLE_QA::Framework::OLELS::Bib_Editor.new(@ole)
+      bib_editor.open
+      create_bib(bib_editor,bib_ary)
+
     end
   end
 end
