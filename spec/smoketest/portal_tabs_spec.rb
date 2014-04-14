9a9d862fdb2cf06c9e1744432ea603ea00399c8c
Jain Waldrip <jkwaldri@iu.edu>
2014-04-14 10:18:09 -0400

    Add portal tab test

---
+++ spec/smoketest/portal_tabs_spec.rb
@@ -0,0 +1,56 @@
+#  Copyright 2005-2013 The Kuali Foundation
+#
+#  Licensed under the Educational Community License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#    http://www.opensource.org/licenses/ecl2.php
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+require 'rspec'
+require 'spec_helper.rb'
+
+describe 'The OLE Portal' do
+  include_context 'Smoketest'
+
+  let(:page)      {OLE_QA::Framework::Page.new(@ole,@ole.url)}
+
+  it 'opens in a browser' do
+    expect(page.open).to be_true
+  end
+
+  it 'has an admin tab' do
+    link = page.b.link(:text => /[Aa]dmin/)
+    expect(link.present?).to be_true
+    link.click
+  end
+
+  it 'has a maintenance tab' do
+    link = page.b.link(:text => /[Mm]aintenance/)
+    expect(link.present?).to be_true
+    link.click
+  end
+
+  it 'has a select/acquire tab' do
+    link = page.b.link(:text => /[Ss]elect.{1}[Aa]cquire/)
+    expect(link.present?).to be_true
+    link.click
+  end
+
+  it 'has a describe tab' do
+    link = page.b.link(:text => /[Dd]escribe/)
+    expect(link.present?).to be_true
+    link.click
+  end
+
+  it 'has a deliver tab' do
+    link = page.b.link(:text => /[Dd]eliver/)
+    expect(link.present?).to be_true
+    link.click
+  end
+end
